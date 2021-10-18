c70b8d01e89ecef20b55364ce982ed28715ce488
Diana Castillo <diana.castillo7676@alumnos.udg.mx>
2021-10-17 23:15:21 -0500

    pPÃagina de inicio

---
+++ libros.js
@@ -0,0 +1,260 @@
+var datos = [
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786070/978607077901.JPG",
+        "titulo": "Flush",
+        "costo": "$168.00",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/flush_30980134",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607380641.JPG",
+        "titulo": "Heist",
+        "costo": "$349.80",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/heist_03120548",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607318120.JPG",
+        "titulo": "Silencio",
+        "costo": "$322.87",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/silencio_38130024",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786077/978607747649.JPG",
+        "titulo": "Tribus",
+        "costo": "$149.00",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/tribus_08512572",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607319333.JPG",
+        "titulo": "Coraline",
+        "costo": "$289.00",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/coraline_43010031",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786075/978607529381.JPG",
+        "titulo": "Crescendo",
+        "costo": "$322.87",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/crescendo_38130009",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9788490/978849070140.JPG",
+        "titulo": "Finale",
+        "costo": "$256.47",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/finale_51903680",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786077/978607748271.JPG",
+        "titulo": "Ariadna",
+        "costo": "$340.00",
+        "categoria": "literatura",
+        "link": "https://www.gonvill.com.mx/libro/ariadna_16320249",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9788420/978842066979.JPG",
+        "titulo": "La Caída",
+        "costo": "$219.30",
+        "categoria": "literatura",
+        "link": "https://www.gonvill.com.mx/libro/la-caida_00203529",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607380559.JPG",
+        "titulo": "Crepúsculo",
+        "costo": "$329.00",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/crepusculo_43010099",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786078/978607871286.JPG",
+        "titulo": "En el Bosque",
+        "costo": "$319.00",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/crepusculo_43010099",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9789706/978970643444.JPG",
+        "titulo": "Robin Hood",
+        "costo": "$59.25",
+        "categoria": "infantiles",
+        "link": "https://www.gonvill.com.mx/libro/robin-hood_00701361",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9789706/978970643869.JPG",
+        "titulo": "Drácula",
+        "costo": "$59.25",
+        "categoria": "infantiles",
+        "link": "https://www.gonvill.com.mx/libro/dracula_00706771",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9789708/978970803030.JPG",
+        "titulo": "Cenicienta",
+        "costo": "$59.25",
+        "categoria": "infantiles",
+        "link": "https://www.gonvill.com.mx/libro/cenicienta_00706988",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9789708/978970803035.JPG",
+        "titulo": "Blanca Nieves",
+        "costo": "$59.25",
+        "categoria": "infantiles",
+        "link": "https://www.gonvill.com.mx/libro/blanca-nieves_00706989",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9788420/978842067426.JPG",
+        "titulo": "El Capital",
+        "costo": "$317.34",
+        "categoria": "política",
+        "link": "https://www.gonvill.com.mx/libro/el-capital_00203554",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786077/978607747505.JPG",
+        "titulo": "Del Tener al Ser",
+        "costo": "$205.54",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/del-tener-al-ser_18104926",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607314850.JPG",
+        "titulo": "Libera tu Magia",
+        "costo": "$319.00",
+        "categoria": "autoayuda",
+        "link": "https://www.gonvill.com.mx/libro/libera-tu-magia_00103418",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9788420/978842066565.JPG",
+        "titulo": "Crimen y Castigo",
+        "costo": "$368.94",
+        "categoria": "literatura",
+        "link": "https://www.gonvill.com.mx/libro/crimen-y-castigo_00203415",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9788420/978842066987.JPG",
+        "titulo": "Hojas de Hierba",
+        "costo": "$279.50",
+        "categoria": "literatura",
+        "link": "https://www.gonvill.com.mx/libro/hojas-de-hierba_00203450",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9788420/978842066498.JPG",
+        "titulo": "Madame Bovary",
+        "costo": "$262.30",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/madame-bovary_00203419",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607380631.JPG",
+        "titulo": "El Italiano",
+        "costo": "$499.00",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/el-italiano_26802644",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607380555.JPG",
+        "titulo": "Tu Mundo y el Mío",
+        "costo": "$349.00",
+        "categoria": "para todos",
+        "link": "hhttps://www.gonvill.com.mx/libro/tu-mundo-y-el-mio_25607639",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786075/978607569112.JPG",
+        "titulo": "Huellas",
+        "costo": "$299.00",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/huellas_19210898",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607319660.JPG",
+        "titulo": "¡Hazlo!",
+        "costo": "$109.00",
+        "categoria": "autoayuda",
+        "link": "https://www.gonvill.com.mx/libro/hazlo_51904371",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607380581.JPG",
+        "titulo": "Anne Frank",
+        "costo": "$249.00",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/anne-frank-diario_25607645",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786070/978607077903.JPG",
+        "titulo": "Las Hijas de Tara",
+        "costo": "$228.00",
+        "categoria": "para todos",
+        "link": "https://www.gonvill.com.mx/libro/las-hijas-de-tara_08512873",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607314575.JPG",
+        "titulo": "Omnia",
+        "costo": "$264.77",
+        "categoria": "juveniles",
+        "link": "hhttps://www.gonvill.com.mx/libro/omnia_03120383",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607317291.JPG",
+        "titulo": "El Secreto de Xein",
+        "costo": "$281.37",
+        "categoria": "juveniles",
+        "link": "https://www.gonvill.com.mx/libro/el-secreto-de-xein_03120454",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607319803.JPG",
+        "titulo": "Línea de Fuego",
+        "costo": "$549.00",
+        "categoria": "literatura",
+        "link": "https://www.gonvill.com.mx/libro/linea-de-fuego_26802570",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9781984/978198481731.JPG",
+        "titulo": "Spark of Light",
+        "costo": "$427.50",
+        "categoria": "Otros Idiomas",
+        "link": "https://www.gonvill.com.mx/libro/spark-of-light_29401742",
+        "cantidad": 10
+    },
+    {
+        "imagen": "https://www.gonvill.com.mx/imagenes/9786073/978607316609.JPG",
+        "titulo": "El Bestiario de Axlin",
+        "costo": "$314.57",
+        "categoria": "fantasía",
+        "link": "https://www.gonvill.com.mx/libro/el-bestiario-de-axlin_03120435",
+        "cantidad": 10
+    }
+];
+
+var carrito = [];
